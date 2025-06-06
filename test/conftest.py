aa83be76f709abc4845bc4e398522b61f01c07b3
Akash Levy <akashlevy@gmail.com>
2025-06-06 16:17:24 -0700

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -8,5 +8,5 @@ def pytest_configure():
     pytest.test_root = Path(__file__).resolve().parent
     pytest.tests = [os.path.basename(el) for el in glob.glob(f"{pytest.test_root}/*.ref.json")]
     pytest.l2j = os.getenv(
-        "PYTEST_L2J_BIN", pytest.test_root.parent / "build" / "src" / "liberty2json"
+        "PYTEST_L2J_BIN", pytest.test_root.parent / "build" / "liberty2json"
     )
