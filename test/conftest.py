311a188314c417e101ae04d801a9f15085ffaf9c
Mohamed Gaber <me@donn.website>
2025-11-24 07:30:22 +0200

    feat: add src attribute, regenerate test cases

--- test/conftest.py
+++ test/conftest.py
@@ -6,7 +6,9 @@ from pathlib import Path
 
 def pytest_configure():
     pytest.test_root = Path(__file__).resolve().parent
-    pytest.tests = [os.path.basename(el) for el in glob.glob(f"{pytest.test_root}/*.ref.json")]
+    pytest.tests = [
+        os.path.basename(el) for el in glob.glob(f"{pytest.test_root}/*.ref.json")
+    ]
     pytest.l2j = os.getenv(
         "PYTEST_L2J_BIN", pytest.test_root.parent / "build" / "liberty2json"
     )
